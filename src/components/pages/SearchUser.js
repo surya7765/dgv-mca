5f373ffb66388417102f5fe285805a1b81b31a46
= <=>
2025-01-30 16:15:11 +0530

    added search feature

--- src/components/pages/SearchUser.js
+++ src/components/pages/SearchUser.js
@@ -1,17 +1,46 @@
-import { TextField } from '@mui/material';
-import React from 'react'
+import { Card, CardContent, CardMedia, CircularProgress, TextField, Typography } from '@mui/material';
+import React, { useEffect } from 'react'
+import Grid from '@mui/material/Grid2';
 
 function SearchUser() {
 
     const [news, setNews] = React.useState([]);
     let [searchText, setSearchText] = React.useState("");
-    let [loading, setLoading] = React.useState(false);
+    let [loading, setLoading] = React.useState(true);
+    const [filteredNews, setFilteredNews] = React.useState([]);
 
-    const searchUser = async () => {
-        const NEWS_API = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=c46ac2ca763445b7890477ecaebeae99"
-        const res = await fetch(NEWS_API);
-        const data = await res.json();
-        setNews(data.articles);
+    useEffect(() => {
+      const fetchNews = async () => {
+        try {
+          const NEWS_API = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=c46ac2ca763445b7890477ecaebeae99"
+          const res = await fetch(NEWS_API);
+          const data = await res.json();
+          setNews(data.articles);
+          setFilteredNews(data.articles);
+          setLoading(false);
+        } catch (error) {
+          console.log("No News Found, error message: ",error);
+        } finally {
+          setLoading(false);
+        }
+      }
+      fetchNews();
+    }, []);
+    
+
+    
+
+    const handleChange = (event) => {
+      const term = event.target.value;
+      setSearchText(term);
+
+      if(term.trim() === ''){
+        setFilteredNews(news);
+      } else{
+        const filtered = news.filter((item) => item.title.toLowerCase().includes(term.toLowerCase()));
+        console.log(filtered);
+        setFilteredNews(filtered);
+      }
     }
 
   return (
@@ -24,13 +53,32 @@ function SearchUser() {
           onChange={handleChange}
           margin='normal'
           />
-        <button onClick={searchUser}>Search</button>
         {
-          news.map((item) => {
-            return (
-              <div>{item.title}</div>
-            );
-          })
+          loading ? (
+            <div>
+              <CircularProgress/>
+            </div>
+          ) : (
+            <div>
+              <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 3, md: 4 }}>
+                {
+                  filteredNews.map((value, index) => {
+                    return (
+                      <Grid item key={index} size={{md:4, xs:6}} >
+                        <Card>
+                          <CardMedia sx={{height:'200px'}} image={value.urlToImage} />
+                          <CardContent>
+                            <Typography variant='h4'>{value.title}</Typography>
+                            <Typography variant='body2'>{value.description}</Typography>
+                          </CardContent>
+                        </Card>
+                      </Grid>
+                    );
+                  })
+                }
+              </Grid>
+            </div>
+          )
         }
     </div>
   )
