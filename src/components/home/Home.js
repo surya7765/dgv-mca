757fa2e91201a4f40532f4b18729791740a0cfd6
= <=>
2025-01-30 16:28:22 +0530

    updated

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,5 @@
-import Counter from '../counter/Counter'
-import FetchApi from '../pages/FetchApi';
+// import Counter from '../counter/Counter'
+// import FetchApi from '../pages/FetchApi';
 import SearchUser from '../pages/SearchUser';
 
 function Home() {
